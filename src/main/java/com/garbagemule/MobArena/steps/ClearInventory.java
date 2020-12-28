228b2313b73d180762b60cb30ba0c28282fd35cf
mrhapo <mrhapo@yahoo.fr>
2020-12-28 11:20:52 +0100

    Update ClearInventory.java

--- src/main/java/com/garbagemule/MobArena/steps/ClearInventory.java
+++ src/main/java/com/garbagemule/MobArena/steps/ClearInventory.java
@@ -19,45 +19,21 @@ class ClearInventory extends PlayerStep {
 
     private ClearInventory(Player player, Arena arena) {
         super(player);
-        this.inventories = new File(arena.getPlugin().getDataFolder(), "inventories");
         this.arena = arena;
     }
 
     @Override
     public void run() {
-        contents = player.getInventory().getContents();
-        createBackup();
-
-        player.getInventory().clear();
     }
 
     @Override
     public void undo() {
-        player.getInventory().setContents(contents);
-
-        arena.getInventoryManager().remove(player);
-        deleteBackup();
     }
 
     private void createBackup() {
-        YamlConfiguration yaml = new YamlConfiguration();
-        yaml.set("contents", contents);
-
-        backup = new File(inventories, player.getUniqueId().toString());
-        try {
-            yaml.save(backup);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to store inventory for " + player.getName(), e);
-        }
-        arena.getInventoryManager().put(player, contents);
     }
 
     private void deleteBackup() {
-        try {
-            Files.delete(backup.toPath());
-        } catch (IOException e) {
-            arena.getPlugin().getLogger().log(Level.WARNING, "Couldn't delete backup inventory file for " + player.getName(), e);
-        }
     }
 
     static StepFactory create(Arena arena) {
